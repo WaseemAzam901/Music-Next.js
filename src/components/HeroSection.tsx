93c5e2560a54e06086646ac3522d1827b895d12e
WaseemAzam901 <getwaseemazam@gmail.com>
2024-08-13 20:51:13 +0400

    Hero Section Almost Done Responsive Remains

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,18 +1,30 @@
 import Link from 'next/link'
 import React from 'react'
-
+import { Spotlight } from './ui/Spotlight'
+import { Button } from './ui/moving-border'
 const HeroSection = () => {
   return (
+    <>
+    
     <div className='h-screen flex items-center justify-center'>
+          <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />    
         <div className='p-4 text-center'>
             <h1 className='mt-20 md:mt-0 md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Master the art of music</h1>
             <p className='font-normal mt-4 text-neutral-300 text-base md:text-lg max-w-lg mx-auto'>Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
-            <div className='mt-4'>
+            <div className='mt-4 text-white'>
             <Link href={"/courses"}>
-            Explore Courses</Link>
+            <Button
+        borderRadius="1.75rem"
+        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+      >
+            Explore Courses </Button></Link>
             </div>
         </div>
     </div>
+    </>
   )
 }
 
